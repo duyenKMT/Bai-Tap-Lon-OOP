ce076280e94751e842f5d3fc43d932d0ae64d3d2
Trần Duyên <73264487+duyenKMT@users.noreply.github.com>
2020-10-24 10:03:33 +0700

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -5,7 +5,56 @@ using namespace std;
 //tao class hoac struct ngay thang nam sinh, nen de la struct cho gon va tien, phan nay Duyen lam
 struct dob
 {
-};
+    int ngay,thang,nam;
+	void nhaptime(){
+        cout<<"lan luot nhap nam,thang,ngay:\n";
+		cout<<"nam: ";cin>>nam;
+		do{
+			cout<<"thang: ";cin>>thang;	
+		}while(thang<1 || thang>12);
+		
+		switch(thang){
+			case 2:
+			{
+				if((nam%400==0) ||(nam%4==0 && nam%100!=0)){
+					do{
+						cout<<"ngay: ";cin>>ngay;
+					}
+					while(ngay<1 || ngay>29);}
+				else
+					do{
+						cout<<"ngay: ";cin>>ngay;
+					}
+					while(ngay<1 || ngay>28);
+				}
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+			{
+				do{
+					cout<<"ngay: ";
+					cin>>ngay;
+				}
+				while(ngay<1 || ngay>30);
+				break;
+			}
+			default:
+			{
+				do{
+					cout<<"ngay: ";
+					cin>>ngay;
+				}
+				while(ngay<1 || ngay>31);
+				break;
+			}	
+		}
+	}
+	void xuattime(){
+		cout<<"\ntime: "<<ngay<<"/"<<thang<<"/"<<nam;
+	}
+} time;
 
 //tao kieu du lieu liet ke ten la gioi tinh (gender)
 enum gender
